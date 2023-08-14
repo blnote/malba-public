240d3b1666edce96da85a2f7abb80326b8da7b6a
B <B@S>
2023-08-14 09:41:12 +0200

    exclude gephi toolkit from jar, updated manifest

--- build.clj
+++ build.clj
@@ -4,9 +4,10 @@
 ;build command: clojure -T:build jar
 
 (def lib 'tu-berlin/malba)
-(def version "0.5")
+(def version "0.5.0")
 (def class-dir "target/classes")
-(def basis (b/create-basis {:project "deps.edn" :aliases []}))
+(def basis (b/create-basis {:project "deps.edn" :aliases [:toolkit :base]}))
+(def uberbasis (b/create-basis {:project "deps.edn" :aliases [:base]}))
 (def uber-file (format "target/%s-%s.jar" (name lib) version))
 
 (defn clean [_]
@@ -19,14 +20,15 @@
                :target-dir class-dir})
   (b/copy-file {:src "malba-algo.edn" :target "target/malba-algo.edn"})
   (b/copy-file {:src "database.edn" :target "target/database.edn"})
-  (b/copy-file {:src "README.md" :target "target/README.md"})
-  
+  (b/copy-file {:src "README.md" :target "target/README.md"}) 
+  (b/copy-file {:src "LICENSE" :target "target/LICENSE"})
   (b/compile-clj {:basis basis
                   :src-dirs ["src"]
                   :class-dir class-dir})
   (b/uber {:class-dir class-dir
            :uber-file uber-file
-           :basis basis
+           :basis uberbasis
+           :manifest {"Class-Path" "gephi-toolkit-0.10.0-all.jar"}
            :main 'malba.core 
            :conflict-handlers {"(?i)^(META-INF/)?(COPYRIGHT|NOTICE|LICENSE)(\\.(txt|md))?$" :append-dedupe}})
   (b/delete {:path "target/classes"})
