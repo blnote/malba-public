983d330e493bcc25e441eb180f3029543228f1d8
B <B@S>
2025-01-30 11:25:28 +0100

    removed prepared statements, parallelized db queries

--- src/malba/database.clj
+++ src/malba/database.clj
@@ -9,59 +9,38 @@
    [malba.logger :as l]
    [next.jdbc :as jdbc]
    [next.jdbc.prepare :as jdbc-p]
+   [next.jdbc.connection :as connection]
    [next.jdbc.result-set :as jdbc-rs]))
 
-(defn- insert-ids
-  "insert ids in sql string from config file by replacing IDLIST"
-  [s ids]
-  (string/replace s "IDLIST" ids))
+(comment 
+  (clojure.repl.deps/add-lib 'com.zaxxer/HikariCP))
 
-(defn- sql-?-string
-  "constructs string (?,?,...,?) with number of question-marks equal to size. used as parameters for prepared statements"
-  [size]
-  (->> (repeat size \?)
-       (interpose \,)
-       string/join
-       (format "(%s)")))
+#_(import (com.zaxxer.hikari HikariDataSource))
 
-(defn- sql-id-string
-  "transform ids into string ('id1','id2',...) used in detail sql query."
-  [ids]
-  (->> (map (fn [id] (string/join ["'" id "'"])) ids)
-       (interpose ",")
-       (string/join)
-       (format "(%s)")))
+(defn- prepare-sql [db mode ids]  
+  (let [stmt [(db (-> mode name (str "-sql") keyword)) 
+              (into-array String ids)]]
+    stmt))
+
+(defn- check-connection [ds]
+  (.close (jdbc/get-connection ds)))
 
 (defn connect
   "checks if connected, otherwise connect to database return structure including prepared sql statements"
   [conf]
-  (if (conf :conn)
-    conf
-    (let [conf (assoc conf :jdbcUrl (string/join ["jdbc:postgresql://" (conf :url)]))
-          conn (jdbc/get-connection conf)]
-      (l/debug "reconnecting to db")
-      (-> conf
-          (assoc :conn conn)
-          (assoc :prepared-cites
-                 (mapv (fn [size]
-                         (let [sql-str (insert-ids (conf :cites-sql) (sql-?-string size))]
-                           (jdbc/prepare conn [sql-str]
-                                         {:timeout (conf :sql-timeout-in-seconds)}))) (conf :batch-sizes)))
-          (assoc :prepared-cited-by
-                 (mapv (fn [size]
-                         (let [sql-str (insert-ids (conf :cited-by-sql) (sql-?-string size))]
-                           (jdbc/prepare conn [sql-str]
-                                         {:timeout (conf :sql-timeout-in-seconds)}))) (conf :batch-sizes)))
-          (#(do (l/debug "reconnected!") %))))))
+  (if-let [ds (conf :ds)]
+    (do (check-connection ds) conf)
+    (let [ds (-> conf 
+                 (assoc :jdbcUrl (string/join ["jdbc:postgresql://" (conf :url)])) 
+                 (jdbc/get-datasource))]
+      (check-connection ds)
+      (l/debug "reconnected to db") 
+      (assoc conf :ds ds)
+      )))
 
 
-
-(defn close! "close db connection if existent" [conf]
-  (when conf
-    (if-let [^java.sql.Connection conn (conf :conn)]
-      (do (.close conn)
-          (dissoc conf :conn))
-      conf)))
+(defn close! "remove datasource" [conf]
+  (dissoc conf :ds))
 
 (defn- add-detail-labels
   "add labels to detail structure whenever pubyear and author is available"
@@ -93,55 +72,55 @@
 (defn- fetch-details-main
   "fetch publication info from items table given db configuration
    for keys in details map" 
-  [db details]
+  [{:keys [ds sql-timeout-in-seconds] :as db} details]
   (l/debug "fetching main details...")
-  (let [{:keys [conn details-sql sql-timeout-in-seconds]} db
-        id-str (sql-id-string (keys details))
-        sql (insert-ids details-sql id-str)
-        rs (jdbc/execute! conn [sql]
+  (with-open [conn
+              (jdbc/get-connection ds)] 
+    (let [stmt (prepare-sql db :details (keys details)) 
+          plan (jdbc/plan conn stmt
                           {:builder-fn jdbc-rs/as-unqualified-maps
-                           :time-out sql-timeout-in-seconds})]
-    (reduce (fn [res {:keys [item_id item_title pubyear source_title]}]
-              (update res item_id #(cond-> %
-                                     item_title (assoc :title item_title)
-                                     pubyear (assoc :pubyear (str pubyear))
-                                     source_title (assoc :source_title source_title)))) details rs)))
+                           :timeout sql-timeout-in-seconds})]
+      (reduce (fn [res {:keys [item_id item_title pubyear source_title]}] 
+                (update res item_id #(cond-> %
+                                       item_title (assoc :title item_title)
+                                       pubyear (assoc :pubyear (str pubyear))
+                                       source_title (assoc :source_title source_title)))) details plan))))
 (defn- fetch-details-authors
   "fetch author info from authors table given db configuration
    for keys in details map" 
-  [db details]
+  [{:keys [ds sql-timeout-in-seconds] :as db} details]
   (l/debug "fetching author infos...")
-  (let [{:keys [conn authors-sql sql-timeout-in-seconds]} db
-        id-str (sql-id-string (keys details))
-        sql (insert-ids authors-sql id-str)
-        rs (jdbc/execute! conn [sql]
-                          {:builder-fn jdbc-rs/as-unqualified-maps
-                           :time-out sql-timeout-in-seconds})]
-    (reduce (fn [res {:keys [item_id auts]}]
-              (update res item_id merge (shortened-authors auts))) details rs)))
+  (with-open [conn
+              (jdbc/get-connection ds)]
+    (let [stmt (prepare-sql db :authors (keys details))
+          rs (jdbc/execute! conn stmt
+                            {:builder-fn jdbc-rs/as-unqualified-maps
+                             :timeout sql-timeout-in-seconds})]
+      (reduce (fn [res {:keys [item_id auts]}]
+                (update res item_id merge (shortened-authors auts))) details rs))))
 
 
 (defn- fetch-details-missing
   "try to obtain publication details from ref table for ids not found in items table"
-  [db details]
+  [{:keys [ds sql-timeout-in-seconds]:as db} details]
   (l/debug "fetching missing publication details...")
-  (let [missing (->> details (filter #(empty? (val %))) (map key))
-        aut-tf (fn [^java.sql.Array ref_auts]
-                 (when-let [s (first (.getArray ref_auts))]
-                   {:authors (string/replace s #"[\{\"'\} ]" "")}))]
-    (if (empty? missing) details
-        (let [{:keys [conn details-from-refs-sql sql-timeout-in-seconds]} db
-              id-str (sql-id-string missing)
-              sql (insert-ids details-from-refs-sql id-str)
-              rs (jdbc/execute! conn [sql]
-                                {:builder-fn jdbc-rs/as-unqualified-maps
-                                 :time-out sql-timeout-in-seconds})]
-          (reduce (fn [res {:keys [item_id_cited,ref_item_title,ref_source_title,ref_authors,ref_pubyear]}]
-                    (assoc res item_id_cited (cond-> {}
-                                               ref_item_title (assoc :title ref_item_title)
-                                               ref_source_title (assoc :source_title ref_source_title)
-                                               ref_pubyear (assoc :pubyear (str ref_pubyear))
-                                               ref_authors (merge (aut-tf ref_authors))))) details rs)))))
+  (with-open [conn
+              (jdbc/get-connection ds)]
+    (let [missing (->> details (filter #(empty? (val %))) (map key))
+          aut-tf (fn [^java.sql.Array ref_auts]
+                   (when-let [s (first (.getArray ref_auts))]
+                     {:authors (string/replace s #"[\{\"'\} ]" "")}))]
+      (if (empty? missing) details
+          (let [stmt (prepare-sql db :details-from-refs missing)
+                rs (jdbc/execute! conn stmt
+                                  {:builder-fn jdbc-rs/as-unqualified-maps
+                                   :timeout sql-timeout-in-seconds})]
+            (reduce (fn [res {:keys [item_id_cited,ref_item_title,ref_source_title,ref_authors,ref_pubyear]}]
+                      (assoc res item_id_cited (cond-> {}
+                                                 ref_item_title (assoc :title ref_item_title)
+                                                 ref_source_title (assoc :source_title ref_source_title)
+                                                 ref_pubyear (assoc :pubyear (str ref_pubyear))
+                                                 ref_authors (merge (aut-tf ref_authors))))) details rs))))))
 
 
 (defn fetch-details
@@ -155,52 +134,50 @@
              (fetch-details-main db)
              (fetch-details-authors db)
              (fetch-details-missing db)
-             (add-detail-labels)
-             (#(do (l/debug "fetching details done.") %))))))
+             (add-detail-labels)))))
 
+
+(defn- process-citation-resultsets [plan mode]
+  (l/debug "processing results...") 
+  (reduce (fn [res {:keys [^String item_id_cited ^String item_id_citing]}]
+            (if (and item_id_cited item_id_citing)
+              (let [entry (if (= mode :cites)
+                            {(.intern item_id_citing) #{(.intern item_id_cited)}}
+                            {(.intern item_id_cited) #{(.intern item_id_citing)}})]
+                (merge-with set/union res entry))
+              res))
+          {} plan))
 
 (defn fetch-citations
   "get map of citation data for a set of ids from database using prepared statements. mode can either be :cites or :cited-by. throws IllegalArgumentException whenever size of ids > max-query-size."
-  [db mode ids] 
-  (let [{:keys [max-query-size batch-sizes prepared-cites prepared-cited-by]} (connect db)]
-    (l/debug "fetching citations")
-    (if (> (count ids) max-query-size)
-      (throw (new IllegalArgumentException (format "SQL larger than MAX-QUERY-SIZE: (%d > %d) " (count ids) max-query-size)))
-      (loop [ids ids
-             C (into {} (map (fn [id] [id #{}])) ids)] ;;add every id as key to avoid repeated lookups
-        (if (empty? ids) C ;nothing to do
-            (let [;find idx of smallest batch size > than number of ids to cache  
-                  batch-idx (or (->> (map-indexed vector batch-sizes)
-                                     (filter #(> (last %) (count ids)))
-                                     ffirst)
-                                (dec (count batch-sizes)))
-                  batch-sz (batch-sizes batch-idx)
-                  cur-ids (take batch-sz ids)
-                ;prepare prepared statement corresponding to batch size
-                  stmt (-> (if (= mode :cites)
-                             (prepared-cites batch-idx)
-                             (prepared-cited-by batch-idx))
-                           (jdbc-p/set-parameters
-                            (concat cur-ids
-                                    (repeat (- batch-sz (count cur-ids)) ""))))
-                  _ (l/debug (format "executing sql statement with batch size %d for ids %d" 
-                                     batch-sz 
-                                     (count ids) ))
-                  m (->> (jdbc/execute! stmt nil {:builder-fn jdbc-rs/as-unqualified-maps})
-                         (#(do (l/debug "results received...") %))
-                         (filter #(and (% :item_id_cited) (% :item_id_citing)))
-                         (reduce (fn [res {:keys [^String item_id_cited ^String item_id_citing]}]
-                                   (let [entry (if (= mode :cites)
-                                                 {(.intern item_id_citing) #{(.intern item_id_cited)}}
-                                                 {(.intern item_id_cited) #{(.intern item_id_citing)}})]
-                                     (merge-with set/union res entry)))
-                                 {}))]
-              (l/debug "processing result")
-              (recur (drop batch-sz ids) (merge-with set/union C m))))))))
+  [{:keys [ds sql-timeout-in-seconds] :as db} mode ids] 
+  (let [{:keys [max-query-size max-batch-size]} (connect db)
+        n (count ids)]
+    (l/debug "fetching citations") 
+    (cond 
+      (= n 0) {} ;nothing to do
+      (> n max-query-size)
+      (throw (new IllegalArgumentException (format "SQL larger than MAX-QUERY-SIZE: (%d > %d) " (count ids) max-query-size))) 
+      :else 
+      (let [;;add every id as key to avoid repeated lookups  
+            C (into {} (map (fn [id] [id #{}])) ids)]
+        (->> (partition-all (or max-batch-size 1000) ids)
+             (pmap (fn [ids]
+                     (let [stmt (prepare-sql db mode ids)]
+                       (with-open [conn
+                                   (jdbc/get-connection ds)]
+                         (-> (jdbc/plan conn
+                                        stmt  {:builder-fn jdbc-rs/as-unqualified-maps
+                                               :timeout sql-timeout-in-seconds})
+                             (process-citation-resultsets mode))))))
+             (apply merge-with set/union C)
+             (doall))))))
 
 
 (defn to-stream! [^java.io.ObjectOutputStream out conf]
-  (.writeObject out (-> conf (dissoc :conn) (dissoc :prepared-cites) (dissoc :prepared-cited-by))))
+  (.writeObject out (-> conf (dissoc :ds) (dissoc :prepared-cites) (dissoc :prepared-cited-by))))
 
 (defn from-stream [^java.io.ObjectInputStream in]
   (.readObject in))
+
+
